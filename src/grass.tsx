daaee188226b7afc50ba0749220200273c469e71
Mandri0n <83769298+Mandri0n@users.noreply.github.com>
2021-08-06 13:23:04 +0200

    Delete grass.tsx

--- src/grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="grass" tilewidth="32" tileheight="32" tilecount="70" columns="10">
- <image source="grass.png" width="320" height="224"/>
-</tileset>
