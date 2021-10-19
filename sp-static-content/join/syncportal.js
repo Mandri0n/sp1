a02913a8ed175176930222633772ac430cc8d94e
LazyProphet <benjamin.ludwig@gmail.com>
2021-10-19 09:39:58 +0000

    Deploying to gh-pages from master @ 2c13ec2c10dddd58f8a7b0210c6e000f5a931700 🚀

---
+++ sp-static-content/join/syncportal.js
@@ -0,0 +1,16 @@
+function openMap(mapurl) {
+    WA.nav.goToRoom(mapurl);
+}
+
+function initLinks() {
+    // if(WA) {
+    //     var elems = document.getElementsByClassName("wa_link");
+        
+    //     for (let index = 0; index < elems.length; ++index) {
+    //         const e = elems[index];
+    //         e.href = "javascript:openMap('" + e.href + "')";
+    //         delete e.target;
+    //     }
+    // }
+    
+}
